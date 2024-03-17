2c8287c494bee3c64a47e6ae4278c4ae04949572
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-03-18 00:16:01 +0100

    eslint: fix lint
    add rule to ignore unused vars if starting with underscore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,7 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
