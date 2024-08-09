2303910c06614ebde4e72504494b8cb9102b315e
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-08-09 23:18:51 +0200

    package: fix npm test

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,5 @@
 export default {
-    preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
-    extensionsToTreatAsEsm: ['.ts'],
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
     transform: {
         '^.+\\.tsx?$': [
             'ts-jest',
@@ -12,5 +7,9 @@ export default {
                 useESM: true,
             },
         ],
+    },
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
     },
 };
