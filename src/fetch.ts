dd022d5bddefac519e6e079171b2e8894bd9d379
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-10-08 17:38:57 +0200

    impl fetchStructs

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,8 +2,13 @@
 // when the GraphQL generated API is not enough
 
 import { Driver } from "neo4j-driver";
-import { SymbolFetchResult, SymbolOptions } from "./ogm-types.js";
-import { FETCH_SYMBOLS_QUERY } from "./queries.js";
+import {
+    SymbolFetchResult,
+    SymbolOptions,
+    WinStructFetchResult,
+    WinStructOptions,
+} from "./ogm-types.js";
+import { FETCH_SYMBOLS_QUERY, FETCH_STRUCTS_QUERY } from "./queries.js";
 
 export async function fetch_symbols(
     driver: Driver,
@@ -28,6 +33,43 @@ export async function fetch_symbols(
         });
     } catch (error) {
         console.error("Error fetching symbols:", error);
+        throw error;
+    } finally {
+        await session.close();
+    }
+}
+
+export async function fetch_structs(
+    driver: Driver,
+    blob_hash: string,
+    options: WinStructOptions
+): Promise<WinStructFetchResult[]> {
+    const session = driver.session();
+    try {
+        const result = await session.executeRead((tx) =>
+            tx.run(FETCH_STRUCTS_QUERY, {
+                blob_hash,
+                skip_count: options.offset,
+                limit_count: options.limit,
+            })
+        );
+
+        return result.records.map((record) => {
+            return {
+                name: record.get("struct_name"),
+                size: record.get("struct_props")["size"],
+                kind: record.get("struct_props")["kind"],
+                fields: record.get("fields").map((field: any) => {
+                    return {
+                        name: field["field_name"],
+                        offset: field["field"]["offset"],
+                        data_type: JSON.parse(field["field"]["data_type"]),
+                    };
+                }),
+            };
+        });
+    } catch (error) {
+        console.error("Error fetching structs:", error);
         throw error;
     } finally {
         await session.close();
