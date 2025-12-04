7e2275328b57ffd65ed4f359670707422ab0c223
Mathieu Tarral <mathieu.tarral@protonmail.com>
2025-12-04 22:45:34 +0100

    index: format

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,12 @@ const env = cleanEnv(process.env, {
     AUTH0_AUDIENCE: str({ desc: "Auth0 API audience" }),
     OBJECT_STORAGE_URI: url({ desc: "S3/MinIO object storage endpoint" }),
     RESTRICTED_BRANCH_NAME: str({ desc: "Branch name for restricted blobs" }),
-    MINIO_ACCESS_KEY: str({ desc: "MinIO access key for authenticated requests" }),
-    MINIO_SECRET_KEY: str({ desc: "MinIO secret key for authenticated requests" }),
+    MINIO_ACCESS_KEY: str({
+        desc: "MinIO access key for authenticated requests",
+    }),
+    MINIO_SECRET_KEY: str({
+        desc: "MinIO secret key for authenticated requests",
+    }),
     MINIO_OBJECTS_BUCKET_NAME: str({
         default: "objects",
         desc: "MinIO bucket name for blob storage",
